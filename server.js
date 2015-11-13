764256ed80db100f860f15ac791a4bd71ccf47dd
Sung Kang <sungjk@msn.com>
2015-11-13 11:37:36 -0800

    SDafsadf

--- server.js
+++ server.js
@@ -1,19 +1,20 @@
-var express = require("express");
-var path = require("path");
+var express = require('express');
+var path = require('path');
 var app = express();
 var bodyParser = require('body-parser');
-app.use(express.static(path.join(__dirname, "./static")));
+app.use(express.static(path.join(__dirname, './static')));
 app.use(bodyParser.urlencoded());
 app.set('views', path.join(__dirname, './views'));
 app.set('view engine', 'ejs');
+app.set('port', (process.env.PORT || 6789));
 app.get('/', function(req, res) {
- res.render("index");
+ res.render('index');
 });
-var server = app.listen(6789, function() {
- console.log("listening on port 6789");
+var server = app.listen(app.get('port'), function() {
+  console.log('listening on port', app.get('port'));
 });
 var io = require('socket.io').listen(server);
 io.sockets.on('connection', function (socket) {
-  console.log("SERVER::WE ARE USING SOCKETS!");
+  console.log('SERVER::WE ARE USING SOCKETS!');
   console.log(socket.id);
 });
