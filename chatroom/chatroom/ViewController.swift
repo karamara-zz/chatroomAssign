a2e5f63fbe8a9229ff966077258ac2bc1ec0ec04
Sung Kang <sungjk@msn.com>
2015-11-13 17:06:36 -0800

    secure htttp

--- chatroom/chatroom/ViewController.swift
+++ chatroom/chatroom/ViewController.swift
@@ -9,7 +9,16 @@
 import UIKit
 
 class ViewController: UIViewController {
+    var socket : SocketIOClient?
 
+    @IBOutlet weak var urlLabel: UITextField!
+    @IBAction func connectButtonPressed(sender: UIButton) {
+        socket = SocketIOClient(socketURL: urlLabel.text!)
+        socket?.connect()
+        socket?.on("connect") { data, ack in
+            print("iOS::WE ARE USING SOCKETS!")
+        }
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
